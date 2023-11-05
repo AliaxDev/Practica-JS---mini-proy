7638e6aeead09d002f1e9b346a2609b7d90e74db
AliaxDev <estupi1968@gmail.com>
2023-11-04 23:37:58 -0400

    update

---
+++ 01 - Page Our Reviews/script.js
@@ -0,0 +1,33 @@
+//local reviews data
+//-----------Data 
+const reviews = [
+    {
+      "id": 1,
+      "nombre": "John Doe",
+      "job": "Desarrollador web",
+      "img": "https://via.placeholder.com/150",
+      "review": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ac ultrices tincidunt, nisl nunc tincidunt nunc, id tincidunt nunc nunc id nunc."
+    },
+    {
+      "id": 2,
+      "nombre": "Jane Smith",
+      "job": "Diseñadora gráfica",
+      "img": "https://via.placeholder.com/150",
+      "review": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ac ultrices tincidunt, nisl nunc tincidunt nunc, id tincidunt nunc nunc id nunc."
+    },
+    {
+      "id": 3,
+      "nombre": "Michael Johnson",
+      "job": "Ingeniero de software",
+      "img": 
+        "https://via.placeholder.com/150",
+      "review": 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ac ultrices tincidunt, nisl nunc tincidunt nunc, id tincidunt nunc nunc id nunc."
+    }
+  ]
+
+//------------select DOM Element
+const author = document.getElementById('author');
+const job = document.getElementById('author');
+const info = document.getElementById('author');
+
